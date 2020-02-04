5b76186cbec5299370597e01df3d2665cb7749c2
Hannes <7892467+hannes-dev@users.noreply.github.com>
2020-02-04 13:10:48 +0100

    moved to tui-rs

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,43 @@
 use colored::*;
+use std::io::{stdin, stdout, Error};
+use tui::Terminal;
+use tui::backend::TermionBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+use termion::raw::IntoRawMode;
 use termion::event::Key;
 use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
 
-fn main() {
+
+fn main() -> Result<(), Error> {
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    let terminal_size = termion::terminal_size().unwrap();
+    let stdout = stdout().into_raw_mode()?;
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
+    let terminal_size = termion::terminal_size().unwrap();
     let width = terminal_size.0;
     let heigth = terminal_size.1;
 
-    write!(stdout, "{}{}{}{} - {}",
-           // Clear the screen.
-           termion::clear::All,
-           // Goto (1,1).
-           termion::cursor::Goto(1, 1), 
-           // Hide the cursor.
-           termion::cursor::Hide,
-           terminal_size.0,
-           terminal_size.1,
-        ).unwrap();
-    // Flush stdout (i.e. make the output appear).
-    stdout.flush().unwrap();
+    terminal.hide_cursor()?;
+    terminal.clear()?;
 
-
-
-    let mut down = 1;
-    let mut right = 1;
-
-    
+    terminal.draw(|mut f| {
+        let size = f.size();
+        Block::default()
+            .borders(Borders::ALL)
+            .render(&mut f, size);
+    })?;
 
     for c in stdin.keys() {
-        let movement: (i16, i16) = match c.unwrap() {
+        match c.unwrap() {
             Key::Char('q') => break,
-            Key::Up => (-1, 0),
-            Key::Right => (0, 1),
-            Key::Down => (1, 0),
-            Key::Left => (0, -1),
-            Key => (0, 0),
-        };
-        
-        if  down + movement.0 > 0i16 && down + movement.0 <= terminal_size.1 as i16 {
-            down += movement.0;
+            _ => continue,
         }
-        if right + movement.1 * 2 > 0i16 && right + movement.1 * 2 <= terminal_size.0 as i16 - 1 {
-            right += movement.1 * 2;
-        }
-
-        write!(stdout, "{}{}{}", termion::clear::All, termion::cursor::Goto(right as u16, down as u16), "  ".on_blue()).unwrap();
-        stdout.flush().unwrap();
     }
 
-    // Show the cursor again before we exit.
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
-}
+    terminal.show_cursor()?;
+    terminal.clear()?;
 
-fn startup() {
-
+    Ok(())
 }
